cc2c51247da66648f7c212ecea80094106972c7b
Dean <dhkjones@gmail.com>
2024-08-21 21:54:33 +0200

    refactor: Update PipelineBindGroups class to remove unused code and improve code organization

--- src/render/pipelineBindGroups.ts
+++ src/render/pipelineBindGroups.ts
@@ -118,22 +118,4 @@ export class PipelineBindGroups {
             ]
         });
     }
-
 }
-
-/*
-Here when we initialize the BindGroups class, we can update the pipeline init code so that 
-async initialize() {
-    await this.createScreenPipeline();
-    
-    if (this.currentRenderPass === RenderPass.Default) {
-        await this.createRayTracingPipelines();
-    } else if (this.currentRenderPass === RenderPass.BVHDebug) {
-        await this.createBVHPipeline();
-    } else {
-        console.error("Invalid render pass");
-    }
-}
-    is used and we can maintain which pipelines are needed at which time. 
-
-*/
